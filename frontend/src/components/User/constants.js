ea02e7a151cca5fc8ddfd42cb2a0e6912df1fac0
karthik5656 <61219228+karthik5656@users.noreply.github.com>
2023-05-11 20:31:21 +0530

    Update constants.js
    updated locations

--- frontend/src/components/User/constants.js
+++ frontend/src/components/User/constants.js
@@ -1,14 +1,11 @@
 export const locations = [
-    'Thane',
-    'Mulund',
-    'Nahur',
-    'Bhandup',
-    'Ghatkopar',
-    'Kurla',
-    'Powai',
-    'Sakinaka',
-    'Airoli',
-    'Andheri',
     'Nad',
-    'Maddilapalem','Madhurawada','Kommadi','Vikhroli','Kanjurmarg','Sriharipuram','Gajuwaka','Anakapalli',
+    'Maddilapalem',
+    'Madhurawada',
+    'Kommadi',
+    'Vikhroli',
+    'Kanjurmarg',
+    'Sriharipuram',
+    'Gajuwaka',
+    'Anakapalli'
 ];
